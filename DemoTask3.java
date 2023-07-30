ffde264e7b3e2088cd422ec134f99333a18af4e3
Pasindu Madhushan <pasinduwijenayaka123@gmail.com>
2023-07-30 11:42:48 +0530

    Complete Task 3

---
+++ DemoTask3.java
@@ -0,0 +1,11 @@
+public class DemoTask3 {
+
+    public static void main(String[] args) {
+        int areaCode = 123;
+        int numFirstPart = 456;
+        int numSecondPart = 789;
+
+        System.out.printf("Telephone no- \033[35m(\033[36m%1$d\033[35m)\033[30m%2$d-%3$d \n",areaCode,numFirstPart,numFirstPart);
+    }
+    
+}
