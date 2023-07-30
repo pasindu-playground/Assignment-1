063fbc5b6350cfdfc986d64d717438ce4f1b5ced
Pasindu Madhushan <pasinduwijenayaka123@gmail.com>
2023-07-30 13:27:26 +0530

    complete Task 6-A

---
+++ DemoTask6A.java
@@ -0,0 +1,9 @@
+public class DemoTask6A {
+
+    public static void main(String[] args) {
+
+        
+        System.out.printf("\033[41;1m[ %1$ 8d\033[42;1m%2$d%%  %3$6s]\033[40;1m%3$s  \n",5,0,' ');
+    }
+    
+}
