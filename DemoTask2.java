2ab5e347cceb6c91d4cacd77e11a9151f6ae1c60
Pasindu Madhushan <pasinduwijenayaka123@gmail.com>
2023-07-30 11:30:11 +0530

    Complte Task 2

---
+++ DemoTask2.java
@@ -0,0 +1,8 @@
+public class DemoTask2 {
+
+    public static void main(String[] args) {
+        float myFloat = (float)Math.PI;
+        System.out.printf("Value of PI: %.3f \n",myFloat);
+    }
+    
+}
