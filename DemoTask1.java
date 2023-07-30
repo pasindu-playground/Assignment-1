ef49a64e3e79650c80b0897494cd726b77dcfb07
Pasindu Madhushan <pasinduwijenayaka123@gmail.com>
2023-07-30 11:23:58 +0530

    Complete task 1

---
+++ DemoTask1.java
@@ -0,0 +1,9 @@
+public class DemoTask1 {
+
+    public static void main(String[] args) {
+        float myFloat = 123456.7890f;
+        System.out.printf("Account Balance : $ %,.2f \n",myFloat);
+        
+    }
+    
+}
